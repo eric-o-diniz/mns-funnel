9be6ba28eb0c010b1b9be2427e812ece1c7cd46b
eric-o-diniz <ericdiniz1212@hotmail.com>
2026-07-15 16:25:55 -0700

    Build Missao Novos Sabores funnel

---
+++ app/config.ts
@@ -0,0 +1,7 @@
+export const MAIN_CHECKOUT_URL =
+  "https://pay.hotmart.com/W106307307V?off=y9e7ffr7&checkoutMode=10";
+
+// Replace these three values when Hotmart generates the post-purchase offer links.
+export const UPSELL_CHECKOUT_URL = "#link-hotmart-upsell";
+export const DOWNSELL_CHECKOUT_URL = "#link-hotmart-downsell";
+export const FUNNEL_EXIT_URL = "#link-hotmart-continuar";
